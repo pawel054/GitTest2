a1a528361c387870423a79591ac4ffd0ae792fab
StalowyDestroyer <patryk20050@o2.pl>
2023-12-12 21:11:17 +0100

    Dodanie klasy i mainpage.xaml.cs

---
+++ GitTesty/GitTesty/Produkt.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GitTesty
+{
+    public class Produkt
+    {
+        public string Nazwa { get; set; }
+        public double Cena { get; set; }
+        public int Ilosc { get; set; }
+
+        public Produkt(string nazwa, double cena, int ilosc)
+        {
+            Nazwa = nazwa;
+            Cena = cena;
+            Ilosc = ilosc;
+        }
+
+        public Produkt() { }
+    }
+}
